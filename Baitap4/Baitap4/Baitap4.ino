0eb63f78f87f7b6039381ce72ec0a0a0477d3b2b
khammeexay <63766737+khammeexay@users.noreply.github.com>
2021-10-14 21:34:43 +0700

    Baitap4

---
+++ Baitap4/Baitap4/Baitap4.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+    analogWrite(9,brightness);
+    delay(30);//Wait for 30 millisecond(s)
+  
+    analongWrite (9, brightness);
+    delay (30);//Wait for 30 millisecond(s)
+  }
+  
+}
